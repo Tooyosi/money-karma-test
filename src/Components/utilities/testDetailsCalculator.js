94fae2df8e99ebc750668924d19f71e3d6ad0768
Oluwatoyosi Oyegoke <oluwatoyosi.oyegoke@gmail.com.com>
2021-12-01 10:17:42 +0100

    fix mistakes bug

--- src/Components/utilities/testDetailsCalculator.js
+++ src/Components/utilities/testDetailsCalculator.js
@@ -3,17 +3,18 @@ export const testDetailsCalculator = (originalValue, typedValue) => {
     const wordsArr = typedValue.split(' ').filter((word) => word);
     const words = wordsArr.length;
     let pointsCount = 0
+    let mistakes = 0
+    
     for (let i = 0; i < wordsArr.length; i++) {
         const element = wordsArr[i];
         if (element === originalWordsArr[i]) {
             pointsCount += 1
+        } else {
+            mistakes += 1
         }
     }
 
     const characters = typedValue?.trim()?.replace(/\s/g, '').length;
-    const mistakes = typedValue.split('').reduce((acc, typedChar, index) => {
-        return typedChar !== originalValue[index] ? acc + 1 : acc
-    }, 0)
 
     return { words, characters, mistakes, points: pointsCount }
 }
